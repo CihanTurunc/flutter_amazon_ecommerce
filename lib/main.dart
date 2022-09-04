9ab99254aeeb8b9ea42192b0047a6ea4e1fef64d
cihanturunc@outlook.com <cihan186131>
2022-09-04 23:49:57 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_amazon_ecommerce/constants/global_variables.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Amazon Ecommerce',
+      theme: ThemeData(
+        scaffoldBackgroundColor: GlobalVariables.backgroundColor,
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Hello"),
+        ),
+        body: const Center(child: Text("Flutter Demo HomePage")),
+      ),
+    );
+  }
+}
